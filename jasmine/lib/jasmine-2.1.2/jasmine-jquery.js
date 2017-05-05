a1f9da950331db9c18116bfc3fe32b6c40bce2b6
priii <get.priyab@gmail.com>
2017-05-04 20:29:41 -0700

    edited

--- jasmine/lib/jasmine-2.1.2/jasmine-jquery.js
+++ jasmine/lib/jasmine-2.1.2/jasmine-jquery.js
@@ -26,6 +26,7 @@ LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
+
 (function (root, factory) {
   if (typeof module !== 'undefined' && module.exports && typeof exports !== 'undefined') {
     factory(root, root.jasmine, require('jquery'));
